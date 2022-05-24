1d37cb207ddbf9c25551d1119c73e926d082c799
John Gargalionis <johngargalionis@gmail.com>
2022-05-24 15:30:27 +0200

    Fixed dictionary output

--- PythonForm.m
+++ PythonForm.m
@@ -31,7 +31,12 @@ $R = ")";
 
 PythonForm[x_String] := "'" <> x <> "'";
 PythonForm[x_Integer] := ToString[x];
-PythonForm[x_Association] := Table[PythonForm[ToString[i[[1]]]] <> ":" <> PythonForm[i[[2]]], {i, Normal[x]}];
+PythonForm[x_Association] :=
+  "{" <>
+      Table[
+        PythonForm[ToString[i[[1]]]] <> ":" <> PythonForm[i[[2]]] <> ","
+      , {i, Normal[x]}
+      ] <> "}";
 PythonForm[Rational[a_, b_]] := PythonForm[a] <> "/" <> PythonForm[b];
 PythonForm[Times[a_, b_]] := PythonForm[a] <> " * " <> PythonForm[b];
 PythonForm[Plus[a_, b_]] := $L <> PythonForm[a] <> " + " <> PythonForm[b] <> $R;
